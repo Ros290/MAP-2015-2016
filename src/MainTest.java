1ef89fd3c888f9a7fe4f299cf09c533d96523f85
Ros290 <redrum290@gmail.com>
2016-07-22 18:08:45 +0200

    Modifica a cazzo

--- src/MainTest.java
+++ src/MainTest.java
@@ -11,6 +11,10 @@ public class MainTest
 		Data trainingSet= new Data("C:/Users/Windows 7/Desktop/DIB/Metodi Avanzati di Programmazione/CasoStudio 2015 -  2016/map2/prova.dat");
 		Data trainingSet2= new Data("C:/Users/Windows 7/Desktop/DIB/Metodi Avanzati di Programmazione/CasoStudio 2015 -  2016/map1/servo.dat");
 		
+		/*
+		 * Metto un commento a cazzo, giusto per controllare se github individua la modifica da me effettuata
+		 */
+		
 		RegressionTree tree=new RegressionTree(trainingSet);
 		RegressionTree tree2 = new RegressionTree(trainingSet2);
 		
